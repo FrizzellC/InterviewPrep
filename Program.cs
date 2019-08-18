9eea41326b1f4b7a466dcff9db08c7d92550a886
Chantel_Frizzell <52367496+ChantelFrizzell@users.noreply.github.com>
2019-08-17 22:43:19 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PracticeProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            char[] input = new char[] { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', ' ', ' ' };
+            string result = URLify(input, 11);
+        }
+
+        public static string URLify(char[] input, int occupied)
+        {
+            double numberOfUnusedSpaces = (input.Length - occupied);
+            int spaces = (int)Math.Ceiling(numberOfUnusedSpaces / 2);
+            string result = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (numberOfUnusedSpaces != 0)
+                {
+                    char current = input[i];
+                    if (Char.IsWhiteSpace(current))
+                    {
+                        input[i] = '%';
+                        input = MoveCharsToRight(input, i);
+                        input[i + 1] = '2';
+                        input[i + 2] = '0';
+                        numberOfUnusedSpaces--;
+                    }
+                    else
+                    {
+                        input[i] = input[i];
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            foreach (char c in input)
+            {
+                result += c;
+            }
+            return result;
+        }
+
+        public static char[] MoveCharsToRight(char[] inputArray, int index)
+        {
+
+            for (int i = inputArray.Length - 1; i > index; i--)
+            {
+                inputArray[i] = inputArray[i - 2];
+            }
+            return inputArray;
+        }
+    }
+}
